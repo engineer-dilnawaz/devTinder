2131dcb7035b82171db9be23143a4b4e986b7bb4
Dilnawaz Khan <dilnawaz@tentwenty.me>
2025-11-25 23:01:15 +0500

    exploring CRUD routes /user

--- src/app.js
+++ src/app.js
@@ -6,6 +6,41 @@ app.use("/hello", (req, res) => {
   res.send("Hello Express JS");
 });
 
+app.get("/user", (req, res) => {
+  res.send({
+    firstName: "Dilnawaz",
+    lastName: "Khan",
+  });
+});
+
+app.post("/user", (req, res) => {
+  res.send({
+    firstName: "Dilnawaz - ",
+    lastName: "Khan",
+  });
+});
+
+app.patch("/user", (req, res) => {
+  res.send({
+    firstName: "Dilnawaz ",
+    lastName: "Khan",
+  });
+});
+
+app.put("/user", (req, res) => {
+  res.send({
+    firstName: "Dilnawaz - ",
+    lastName: "Khan",
+  });
+});
+
+app.delete("/user", (req, res) => {
+  res.send({
+    firstName: "Dilnawaz - ",
+    lastName: "Khan",
+  });
+});
+
 const PORT = 3001;
 app.listen(PORT, (error) => {
   if (error) {
